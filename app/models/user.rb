0decbca943634124b7202de7def1cade26edcff3
Timothy Ingram <tim.ringram@gmail.com>
2016-05-21 21:07:38 -0400

    added messaging system with mailboxer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ActiveRecord::Base
   has_many :listings
   has_secure_password
+  acts_as_messageable
 
   def host?
     self.role == 'host'
   end
+
+  def name
+    "User #{:id}"
+  end
+
+  # def mailboxer_email
+  # end
+
+  # def mailboxer_email(object)
+  #   self.email
+  # end
 end
