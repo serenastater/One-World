88ace98909b0433348f28315fe51514bc0b4deb0
Serena Stater <srstater@gmail.com>
2016-05-24 16:14:58 -0400

    added dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,9 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   get '/login' => 'sessions#new'
   post 'login' => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
+  get 'logout' => 'sessions#destroy'
   get 'cities' => 'cities#index'
+  get '/profile' => 'dashboard#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
