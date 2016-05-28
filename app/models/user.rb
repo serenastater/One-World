a98d620e261243749d9fb10a93096f99a6aa5d5c
Timothy Ingram <tim.ringram@gmail.com>
2016-05-28 17:29:48 -0400

    updated links and added avatar icons to mailbox messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :listings
   has_secure_password
   acts_as_messageable
-  has_attached_file :avatar, styles: { icon: "32x32", small: "64x64", med: "100x100", large: "200x200" }, :url => "/assets/users/:id/:style/:basename.:extension", :path => ":rails_root/public/assets/users/:id/:style/:basename.:extension"
+  has_attached_file :avatar, styles: { icon: "36x36", small: "64x64", med: "100x100", large: "200x200" }, :url => "/assets/users/:id/:style/:basename.:extension", :path => ":rails_root/public/assets/users/:id/:style/:basename.:extension"
   do_not_validate_attachment_file_type :avatar
 
   def host?
