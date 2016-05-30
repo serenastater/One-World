14ac72018fb42eb67fd0288add4b1c33e1909593
Serena Stater <srstater@gmail.com>
2016-05-29 21:36:47 -0400

    umm idk anymore

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,8 @@ class User < ActiveRecord::Base
   has_many :listings
   has_secure_password
   acts_as_messageable
-  has_attached_file :avatar, :url => "/assets/users/:id/:style/:basename.:extension", :path => ":rails_root/public/assets/users/:id/:style/:basename.:extension"
+  has_attached_file :avatar, styles: { icon: "36x36", small: "64x64", med: "100x100", large: "200x200" },:url => "/assets/users/:id/:style/:basename.:extension", :path => ":rails_root/public/assets/users/:id/:style/:basename.:extension"
   do_not_validate_attachment_file_type :avatar
-
-  # insert this line after avatar
-  # styles: { icon: "36x36", small: "64x64", med: "100x100", large: "200x200" },
 
   def host?
     self.role == 'Host'
